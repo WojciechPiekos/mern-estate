001b1bfa2c9e5a96cea3e4ad567dfee9264d4ec4
Wojciech Piekos <wojciech.piekos@gmail.com>
2023-10-05 14:18:39 +0200

    mongo connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+
 const PORT = 3000
 
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`)
+connectDB()
+
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB")
+    app.listen(PORT,() => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
+
+mongoose.connection.on('error', error => {
+    console.log(error)
 })
