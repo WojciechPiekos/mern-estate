8c0906b5f0f499a969ebb2337222f9ab1e1a98f7
Wojciech Piekos <wojciech.piekos@gmail.com>
2023-10-10 17:32:28 +0200

    google authentication

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -56,7 +56,62 @@ const signin = async (req, res, next) => {
   }
 };
 
+const google = async (req, res, next) => {
+  const { name, email, photo } = req.body;
+
+  try {
+    const user = await User.findOne({ email }).exec();
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        })
+        .status(200)
+        .json({ rest });
+    } else {
+      const generatedPwd =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+
+      const hashedPwd = bcrypt.hashSync(generatedPwd, 10);
+
+      const username =
+        name.split(" ").join("").toLowerCase() +
+        Math.random().toString(36).split(-4);
+
+        console.log(username, hashedPwd, email, photo)
+
+      const newUser = new User({
+        username,
+        email,
+        password: hashedPwd,
+        avatar: photo,
+      });
+
+      await newUser.save();
+
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+
+      const { password: pass, ...rest } = newUser._doc;
+
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        })
+        .status(200)
+        .json({ rest });
+    }
+  } catch (error) {
+    errorHandler(error, req, res, next);
+  }
+};
+
 module.exports = {
   signup,
   signin,
+  google,
 };
