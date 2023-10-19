72d7c82d8091212461ec9f6547e77b919b5db162
Wojciech Piekos <wojciech.piekos@gmail.com>
2023-10-19 12:23:59 +0200

    create update listing api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -10,26 +10,51 @@ const createListing = async (req, res, next) => {
   }
 };
 
-const deleteListing = async (req,res,next) => {
-  const listing = await Listing.findById(req.params.id)
+const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
 
   if (!listing) {
-    return next(customError(404,'Listing not found',req,res))
+    return next(customError(404, "Listing not found", req, res));
   }
 
   if (req.user.id !== listing.userRef) {
-    return next(customError(401, 'You can only delete your own listings!',req, res))
+    return next(
+      customError(401, "You can only delete your own listings!", req, res)
+    );
   }
 
   try {
-    await Listing.findByIdAndDelete(req.params.id)
-    res.status(200).json({message: "Listing has been deleted!"})
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Listing has been deleted!" });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(customError(404, "Listing not found", req, res));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(
+      customError(401, "You can only update your own litings!", req, res)
+    );
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(customError(error.code,error.message,req,res));
+  }
+};
 
 module.exports = {
   createListing,
   deleteListing,
+  updateListing,
 };
--- api/routes/listingRoutes.js
+++ api/routes/listingRoutes.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createListing, deleteListing } = require("../controllers/listingController");
-const verifyToken = require('../utils/verifyUser');
+const {
+  createListing,
+  deleteListing,
+  updateListing,
+} = require("../controllers/listingController");
+const verifyToken = require("../utils/verifyUser");
 
-
-router.post("/create", verifyToken, createListing)
-router.delete("/delete/:id",verifyToken, deleteListing)
-
-
-
-
-
+router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 module.exports = router;
