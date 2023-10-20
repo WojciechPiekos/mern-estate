2cf25c2c1282c6b9311b88ef83b7dc7ceabaa7b8
Wojciech Piekos <wojciech.piekos@gmail.com>
2023-10-20 17:36:08 +0200

    create search api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -49,25 +49,75 @@ const updateListing = async (req, res, next) => {
     );
     res.status(200).json(updatedListing);
   } catch (error) {
-    next(customError(error.code,error.message,req,res));
+    next(customError(error.code, error.message, req, res));
   }
 };
 
-const getListing = async (req,res,next) => {
+const getListing = async (req, res, next) => {
   try {
-    const listing = await Listing.findById(req.params.id)
+    const listing = await Listing.findById(req.params.id);
     if (!listing) {
-      return next(customError(404,"Listing not found", req, res))
+      return next(customError(404, "Listing not found", req, res));
     }
-    res.status(200).json(listing)
+    res.status(200).json(listing);
   } catch (error) {
-    next(customError(error.code, error.message, req, res))
+    next(customError(error.code, error.message, req, res));
   }
-}
+};
+
+const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["rent", "sale"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listing = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listing)
+
+  } catch (error) {
+    next(customError(error.code, error.message, req, res));
+  }
+};
 
 module.exports = {
   createListing,
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 };
